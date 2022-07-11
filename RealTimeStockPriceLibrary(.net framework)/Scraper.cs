029c9355ff2bd40a5a577eeb9ba756906f0bc06a
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2022-07-11 18:14:05 +0200

    -

---
+++ RealTimeStockPriceLibrary(.net framework)/Scraper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeStockPriceLibrary_.net_framework_
+{
+    public class Scraper
+    {
+        HtmlAgilityPack.HtmlWeb htmlWeb = new HtmlAgilityPack.HtmlWeb();
+        public float GetPrice(string ticker)
+        {
+            HtmlAgilityPack.HtmlDocument htmlDocument = htmlWeb.Load("https://finance.yahoo.com/quote/" + ticker + "?p=" + ticker);
+            string temp = "";
+            foreach (var item in htmlDocument.DocumentNode.SelectNodes("//*[@id=\"quote-header-info\"]/div[3]/div[1]/div/fin-streamer[1]"))
+            {                                                         
+                temp += item.InnerText;
+                temp = temp.Replace(",", "");
+                temp = temp.Replace('.', ',');
+            }
+            try
+            {
+                return float.Parse(temp);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public double GetVolume(string ticker)
+        {
+            HtmlAgilityPack.HtmlDocument htmlDocument = htmlWeb.Load("https://finance.yahoo.com/quote/" + ticker + "?p=" + ticker);
+            string temp = "";
+            foreach (var item in htmlDocument.DocumentNode.SelectNodes("//*[@id=\"quote-summary\"]/div[1]/table/tbody/tr[7]/td[2]/fin-streamer"))
+            {
+                temp += item.InnerText;
+                temp = temp.Replace(",", "");
+                temp = temp.Replace('.', ',');
+            }
+            try
+            {
+                return double.Parse(temp);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
